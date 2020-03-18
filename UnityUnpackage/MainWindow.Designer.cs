f5d891f422f9f71f7e98594854b3d2006004d35a
Tero Korpela <tero.korpela@gmail.com>
2020-03-18 15:44:46 +0200

    Project to git

---
+++ UnityUnpackage/MainWindow.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace UnityUnpackage
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::UnityUnpackage.Properties.Resources.unitypackage_5176;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 150);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Drop .unitypackage";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(132, 150);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "UnityUnpackage";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.DragLeave += new System.EventHandler(this.Form1_DragLeave);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ UnityUnpackage/MainWindow.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
+
+namespace UnityUnpackage
+{
+    public partial class MainWindow : Form
+    {
+        private int assetCounter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragLeave(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if(Path.GetExtension(files[0]).Equals(".unitypackage")) {
+                Console.WriteLine("GotFileDrop" + files[0]);
+                ExtractUnitypackage(files[0]);
+                MessageBox.Show(assetCounter + " assets extracted");
+            }
+        }
+
+        private void ExtractUnitypackage(string filename)
+        {
+            Console.WriteLine("ExtractUnitypackage");
+            var tempFolder = Path.Combine(Path.GetTempPath(), "tmp_" + Path.GetFileNameWithoutExtension(filename));
+            var targetFolder = Path.Combine(Path.GetDirectoryName(filename), Path.GetFileNameWithoutExtension(filename) + "_extracted");
+
+            if (Directory.Exists(tempFolder))
+                Directory.Delete(tempFolder, true);
+            if (Directory.Exists(targetFolder))
+            {
+                MessageBox.Show("Target folder already exists");
+                return;
+            }
+            Directory.CreateDirectory(tempFolder);
+            Directory.CreateDirectory(targetFolder);
+
+            ExtractTGZ(filename, tempFolder);
+            ProcessExtracted(tempFolder, targetFolder);
+
+            Directory.Delete(tempFolder, true);
+        }
+
+        public void ExtractTGZ(string gzArchiveName, string destFolder)
+        {
+            Stream inStream = File.OpenRead(gzArchiveName);
+            Stream gzipStream = new GZipInputStream(inStream);
+
+            TarArchive tarArchive = TarArchive.CreateInputTarArchive(gzipStream);
+            tarArchive.ExtractContents(destFolder);
+            tarArchive.Close();
+
+            gzipStream.Close();
+            inStream.Close();
+        }
+
+        private void ProcessExtracted(string tempFolder, string targetFolder)
+        {
+            assetCounter = 0;
+            foreach (string d in Directory.EnumerateDirectories(tempFolder))
+            {
+                string relativePath = "";
+                string targetFullPath = "";
+                string targetFullFile = "";
+
+                if (File.Exists(Path.Combine(d, "pathname")))
+                {
+                    relativePath = File.ReadAllText(Path.Combine(d, "pathname"));
+                    targetFullPath = Path.GetDirectoryName(Path.Combine(targetFolder, relativePath));
+                    targetFullFile = Path.Combine(targetFolder, relativePath);
+                }
+
+                // If asset file exists
+                if (File.Exists(Path.Combine(d, "asset")))
+                {
+                    Directory.CreateDirectory(targetFullPath);
+                    File.Move(Path.Combine(d, "asset"), targetFullFile);
+                    assetCounter++;
+                }
+
+                // If asset meta file exists
+                if (File.Exists(Path.Combine(d, "asset.meta")))
+                {
+                    Directory.CreateDirectory(targetFullPath);
+                    File.Move(Path.Combine(d, "asset.meta"), targetFullFile + ".meta");
+                }
+
+                // If asset preview file exists
+                if (File.Exists(Path.Combine(d, "preview.png")))
+                {
+
+                }
+            }
+        }
+    }
+}
